d8c072063a5cdebcd30ac3901a0eec9c39e03d4f
supkevo <supkevi@gmail.com>
2026-05-14 20:16:06 +0100

    Update server.js

--- server.js
+++ server.js
@@ -41,8 +41,10 @@ app.get("/status", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
 
 app.get("/", (req, res) => {
